105fe86a427e5ddc482272a459c846cb0f04474f
New_2000 <TanachotNew2543@gmail.com>
2024-02-07 13:19:13 +0700

    implement update and delete book

--- book/book.go
+++ book/book.go
@@ -2,6 +2,7 @@ package book
 
 import (
 	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -25,23 +26,25 @@ var sliceOfBook []Book = []Book{
 	},
 }
 
+// G is upper case. So this is public function
 func GetBookById(name *string) Book {
 
 	fmt.Println(*name)
 
+	// _ is index (if you want to use index change _ to i or something)
 	for _, book := range sliceOfBook {
 		if book.Name == *name {
 			return book
 		}
 	}
 
-	return Book{}
+	// new(Struct) is how to allocate memory address for this.Struct (use *new(Struct) to get value)
+	return *new(Book)
 }
 
 func GetBooks() []Book {
 
 	//  local var scope has (no STATE)
-
 	if len(sliceOfBook) == 0 {
 
 		sliceOfBook = append(sliceOfBook, Book{
@@ -58,4 +61,34 @@ func GetBooks() []Book {
 	}
 
 	return sliceOfBook
+}
+
+func InsertBook(book *Book) []Book {
+	sliceOfBook = append(sliceOfBook, *book)
+	return sliceOfBook
+}
+
+func UpdateBook(updateBook *Book) *string {
+	for index, book := range sliceOfBook {
+		if book.Id == updateBook.Id {
+			sliceOfBook[index].Name = updateBook.Name
+			sliceOfBook[index].Title = updateBook.Title
+
+			return &book.Id
+		}
+	}
+
+	return nil
+}
+
+func DeleteBook(name *string) *string {
+	for index, book := range sliceOfBook {
+		if book.Name == *name {
+			sliceOfBook = append(sliceOfBook[:index], sliceOfBook[index+1:]...)
+
+			return &book.Id
+		}
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -12,13 +12,17 @@ func main() {
 	app := fiber.New()
 
 	app.Get("books", getBooks)
-	app.Get("book/:id", getBookById)
+	app.Get("book/:name", getBookById)
+	app.Post("book", insertBook)
+	app.Put("book", updateBook)
+	app.Delete("book/:name", deleteBook)
 
 	app.Listen(":3000")
 }
 
+// g is lower case. So this is private function
 func getBookById(context *fiber.Ctx) error {
-	name := context.Params("id")
+	name := context.Params("name")
 	var decodedName, err = url.QueryUnescape(name)
 
 	if err != nil {
@@ -34,6 +38,48 @@ func getBookById(context *fiber.Ctx) error {
 func getBooks(context *fiber.Ctx) error {
 	books := book.GetBooks()
 	var result = context.JSON(books)
+
+	return result
+}
+
+func insertBook(context *fiber.Ctx) error {
+	newBook := new(book.Book)
+	error := context.BodyParser(newBook)
+
+	if error != nil {
+		return context.SendStatus(fiber.StatusBadRequest)
+	}
+
+	bookResponse := book.InsertBook(newBook)
+	result := context.JSON(bookResponse)
+
+	return result
+}
+
+func updateBook(context *fiber.Ctx) error {
+	updateBook := new(book.Book)
+	error := context.BodyParser(updateBook)
+
+	if error != nil {
+		return context.SendStatus(fiber.StatusBadRequest)
+	}
+
+	bookResponse := book.UpdateBook(updateBook)
+	result := context.JSON(bookResponse)
+
+	return result
+}
+
+func deleteBook(context *fiber.Ctx) error {
+	name := context.Params("name")
+	var decodedName, err = url.QueryUnescape(name)
+
+	if err != nil {
+		return context.SendStatus(fiber.StatusBadRequest)
+	}
+
+	idResponse := book.DeleteBook(&decodedName)
+	result := context.JSON(idResponse)
 
 	return result
 }
